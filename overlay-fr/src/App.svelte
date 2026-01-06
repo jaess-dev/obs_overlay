68f1c0d490ebfd8fc1b08e6c55c2037752c796ca
jaess-dev <jaess-dev@gmx.de>
2026-01-06 21:42:57 +0100

    init the frontend

---
+++ overlay-fr/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  const name = "JaessDev";
+</script>
+
+<main class="w-screen h-screen bg-transparent">
+  <nav class="fixed top-0 left-0 w-full bg-black/30 backdrop-blur-md">
+    <div class="flex items-center justify-center py-3">
+      <h1
+        class="text-3xl font-bold text-green-400 tracking-wide
+               drop-shadow-[0_0_8px_rgba(34,197,94,0.8)]"
+      >
+        {name}
+      </h1>
+    </div>
+  </nav>
+</main>
+
+<style>
+  :root {
+    background-color: transparent !important;
+    margin: 0;
+    padding: 0;
+  }
+</style>
